8a6fd27574ff83020da3d48e7476b63a9b196537
Ashtam <ashtam@artiosys.com>
2020-07-06 03:27:31 +0530

    controller for HS

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ let disconnectButton = document.getElementById('disconnect');
 let terminalContainer = document.getElementById('terminal');
 let sendForm = document.getElementById('send-form');
 let inputField = document.getElementById('input');
-let serviceUuid;
-let characteristicUuid;
+var serviceUuid = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+var characteristicUuid = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
+var characteristicRX = 'e3327c31-3123-4a99-bf27-b900c24c4e68';
 let lastValue;
-
+var button1on = document.getElementById('sw1_on');
+var button1off = document.getElementById('sw1_off');
+var deviceName;
+var toSend;
 connectButton.addEventListener('click', function() {
   connect();
 });
@@ -14,6 +18,16 @@ connectButton.addEventListener('click', function() {
 disconnectButton.addEventListener('click', function() {
   disconnect();
 });
+
+button1on.addEventListener('click',function(){
+  sendStatus(3,1);
+});
+
+button1off.addEventListener('click',function(){
+  sendStatus(3,0);
+});
+
+
 
 sendForm.addEventListener('submit', function(event) {
   serviceUuid = document.querySelector('#service').value;
@@ -28,6 +42,24 @@ sendForm.addEventListener('submit', function(event) {
   inputField.focus();     
 });
 
+
+
+function sendStatus(switch_id,switch_state){
+  log("device name "+deviceName);
+  var deviceId = String(deviceName).substring(String(deviceName).indexOf("-")+1);
+  log('Device ID : '+ deviceId);
+  toSend = "xxxxx/"+deviceId+"/"+switch_id+"/"+switch_state;
+  // var toSend = "wifi/clear";
+  log('To send : '+ toSend);
+  toSend.trim();
+
+  // return deviceCache.gatt.getPrimaryService(serviceUuid)
+  // .then(service => service.getCharacteristic(characteristicUuid))
+  // .then(characteristic => characteristic.writeValue(toSend));
+  log(toSend.type);
+  send(toSend);
+}
+
 let deviceCache = null;
 
 let characteristicCache = null;
@@ -38,28 +70,27 @@ function connect() {
   return (deviceCache ? Promise.resolve(deviceCache) :
       requestBluetoothDevice()).
       then(device => connectDeviceAndCacheCharacteristic(device)).
-      then(characteristic => startNotifications(characteristic)).
+      then(characteristic => startNotifications(characteristicRX)).
+      // then(characteristic => {
+      //   // Writing 1 is the signal to reset energy expended.
+      //   return characteristic.writeValue(toSend);
+      // }).
       catch(error => log(error));
 }
 
 function requestBluetoothDevice() {
   log('Requesting bluetooth device...');
-
-  
-
-
-
   return navigator.bluetooth.requestDevice({
-    // acceptAllDevices: true,
-    filters: [{services: [serviceUuid]}],
-    // optionalServices: ['beb5483e-36e1-4688-b7f5-ea07361b26a8'],
+    acceptAllDevices: true,
+    // filters: [{namePrefix: 'HS-'}],
+    optionalServices: [serviceUuid],
   }).
       then(device => {
         log('"' + device.name + '" bluetooth device selected');
         deviceCache = device;
         deviceCache.addEventListener('gattserverdisconnected',
             handleDisconnection);
-
+        deviceName = String(device.name);
         return deviceCache;
       });
 }
@@ -108,12 +139,17 @@ function connectDeviceAndCacheCharacteristic(device) {
 function startNotifications(characteristic) {
   log('Starting notifications...');
 
-  return characteristic.startNotifications().
-      then(() => {
-        log('Notifications started');
-        characteristic.addEventListener('characteristicvaluechanged',
-            handleCharacteristicValueChanged);
-      });
+  // return characteristic.startNotifications().
+  //     then(() => {
+  //       log('Notifications started');
+  //       characteristic.addEventListener('characteristicvaluechanged',
+  //           handleCharacteristicValueChanged);
+  //     });
+
+      return deviceCache.gatt.getPrimaryService(serviceUuid)
+      .then(service => service.getCharacteristic(characteristicRX))
+      .then(characteristic => characteristic.startNotifications())
+      .then(characteristic => characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged));
 }
 //Autoscroll
 function pageScroll() {
@@ -193,7 +229,7 @@ function send(data) {
 
   data += '\n';
 
-  if (data.length > 20) {
+  if (data.length > 10) {
     let chunks = data.match(/(.|[\r\n]){1,20}/g);
 
     writeToCharacteristic(characteristicCache, chunks[0]);
